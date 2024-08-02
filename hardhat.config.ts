0560e5f44f23c863e8a0ec3fe5e3932d0b2b756b
Yurii Pasternak <y.pasternak.work@gmail.com>
2024-08-02 21:55:03 +0200

    prepare project

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@openzeppelin/hardhat-upgrades"
+import "@nomicfoundation/hardhat-toolbox";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    localhost: {
+      url: process.env.LOCALHOST_RPC,
+      // @ts-ignore
+      accounts: [process.env.LOCALHOST_PK]
+    },
+  }
+};
+
+export default config;
